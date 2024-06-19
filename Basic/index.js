077abf11f0556b7c735c9278e0c5ca2c2ce21979
Sakin790 <sakinislam79@gmail.com>
2024-06-20 01:13:37 +0600

    added

--- Basic/index.js
+++ Basic/index.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+import express from "express"
+import http from "http"
+import path from "path"
+import { Server } from "socket.io"
+
+
+
+
+
+
+const app = express()
+const port = 8080
+const io= 
+
+app.use(express.static(path.resolve("./public")))
+const server = http.createServer(app)
+
+
+
+
+app.get('/', (req, res) => {
+    return res.sendFile("./public/index.html")
+})
+server.listen(port, () => {
+    console.log(`Server listening on ${port}`);
+})
