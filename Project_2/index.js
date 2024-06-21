db7b1a6e66851f82df8e5782f67dd102f3e29efb
Sakin790 <sakinislam79@gmail.com>
2024-06-21 20:57:06 +0600

    frontend ready

--- Project_2/index.js
+++ Project_2/index.js
@@ -1,1 +1,23 @@
-console.log("Server Running");
+import express from "express"
+import http from "http"
+import path from "path"
+import { Server } from "socket.io"
+
+
+
+
+const app = express()
+const PORT = 9090
+
+
+app.use(express.static(path.resolve("./public")))
+const server = http.createServer(app)
+
+
+app.get("/", (req, res) => {
+    res.sendFile("./public/index.html")
+})
+
+server.listen(PORT, () => {
+    console.log(` Server is running at port ${PORT}`);
+})
